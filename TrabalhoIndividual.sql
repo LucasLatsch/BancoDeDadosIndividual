bae5b1462f4ccd0def3e5b571edc5f7067726ea6
LucasLatsch <bello-lucas@hotmail.com>
2023-08-24 01:25:27 -0300

    Atualização Final

--- TrabalhoIndividual.sql
+++ TrabalhoIndividual.sql
@@ -70,8 +70,40 @@ car_dt_release, car_nm_price, car_tx_plate, fk_prs_cd_id)values
 ('Honda', 'Civic', 'Cinza', 'Gasolina', '20/12/2018', 125000.00, 'FFT6J91', 3),
 ('Toyota', 'Corolla', 'Prata', 'Flex', '16/10/2009', 86000.00, 'ABC1E23', 4),
 ('Subaro', 'WRX', 'Azul', 'Gasolina', '29/09/2016', 125900.00, 'EFG1J60', 5);
+insert into carro(car_tx_brand, car_tx_model, car_tx_color, car_tx_fuel_type,
+car_dt_release, car_nm_price, car_tx_plate, fk_prs_cd_id)values 
+('Fiat', 'Fiero 2000', 'Vermelho', 'Diesel', '20/06/2023', 65800.00, 'FICT999', 1),
+('Fiat', 'Urbano 300', 'Azul Céu', 'Gasolina', '10/06/2023', 52500.00, 'FICT777',1),
+('Fiat', 'LuxoDrive S4', 'Preto Onix', 'Híbrido - Etanol/Elétrico', '16/10/2022', 89.90000, 'FICT555',1),
+('Fiat', 'Punto Easy', 'Branco Neve', 'Gasolina', '02/01/2023', 45500.00, 'FICT444',1);
+insert into carro(car_tx_brand, car_tx_model, car_tx_color, car_tx_fuel_type,
+car_dt_release, car_nm_price, car_tx_plate, fk_prs_cd_id)values
+('Honda', 'Civic LX', 'Cinza Metálico', 'Flex - Gasolina/Etanol', '02/12/2021', 68700.00, 'FICT778',1),
+('Honda', 'Fit EX', 'Azul Oceano', 'Gasolina', '21/02/2023', 57900.00,  'FICT888',2),
+('Honda', 'CR-V Touring', 'Preto Pérola', 'Híbrido - Gasolina/Elétrico', '05/07/2019', 98500.00, 'FICS545',2),
+('Honda', 'HR-V Sport', 'Vermelho Paixão', 'Flex - Gasolina/Etanol', '16/09/2023', 73200.00, 'FICT667',2);
+insert into carro(car_tx_brand, car_tx_model, car_tx_color, car_tx_fuel_type,
+car_dt_release, car_nm_price, car_tx_plate, fk_prs_cd_id)values
+('Toyota', 'Hilux SR', 'Cinza Metálico', 'Diesel', '16/08/2022', 98200.00, 'FICT441',2),
+('Toyota', 'Etios XS', 'Azul Metálico', 'Flex - Gasolina/Etanol', '15/02/2021', 48700.00, 'FICT331',3),
+('Toyota', 'RAV4 XLE', 'Prata Lunar', 'Gasolina',  '03/08/2017', 91500.00, 'FICT231',3),
+('Toyota', 'Corolla LE', 'Branco Pérola', 'Híbrido - Gasolina/Elétrico', '12/02/2021', 82900.00, 'FICT118',3);
+insert into carro(car_tx_brand, car_tx_model, car_tx_color, car_tx_fuel_type,
+car_dt_release, car_nm_price, car_tx_plate, fk_prs_cd_id)values
+('Subaru', 'Impreza Base', 'Azul Safira', 'Gasolina', '12/05/2021', 59800.00, 'FICT559',3),
+('Subaru', 'Forester Premium', ' Verde Floresta', 'Híbrido - Gasolina/Elétrico', '16/05/2022', 79900.00, 'FICT667',3),
+('Subaru', 'Outback Limited', 'Prata Metálico', 'Flex - Gasolina/Etanol', '26/03/2020', 88500.00, 'FICT773',3),
+('Subaru', 'WRX STI', 'Vermelho Rally', 'Gasolina', '12/12/2015', 105200.00, 'FICT884',4);
+insert into carro(car_tx_brand, car_tx_model, car_tx_color, car_tx_fuel_type,
+car_dt_release, car_nm_price, car_tx_plate, fk_prs_cd_id)values
+('Volkswagen', 'Golf Comfortline', 'Prata Reflex', 'Flex - Gasolina/Etanol', '19/07/2018', 72400.00, 'FICT116',4),
+('Volkswagen', 'Polo Highline', 'Azul Noturno', 'Gasolina', '13/06/2020', 59900.00, 'FICT226',4),
+('Volkswagen', 'T-Cross Comfort', 'Vermelho Sunset', 'Híbrido - Gasolina/Elétrico', '12/02/2023', 85700.00, 'FICT123',4),
+('Volkswagen','Amarok Highline', 'Cinza Indium', 'Diesel', '05/01/2023', 12030.00, 'FICT544',5);
+update carro set car_tx_brand  = 'Subaru' where car_tx_brand  like 'Subaro';
+update carro set car_tx_fuel_type  = 'Flex - Gasolina/Etanol' where car_cd_id = 2;
+update carro set car_tx_fuel_type  = 'Flex - Gasolina/Etanol' where car_cd_id = 4;
 
-insert into pessoa(prs_tx_email, prs_tx_senha, prs_tx_name,
 prs_tx_cpf, prs_dt_date_born, prs_int_tel, fk_end_cd_id)values
 ('bello-lucas@hotmail.com', '12345', 'Lucas', '11122233344', '16/10/1996', '992716773', 1),
 ('RianF@hotmail.com', '54321', 'Rian', '44433322211', '20/09/1998', '992718596', 2),
@@ -108,16 +140,20 @@ select p.prs_tx_name, p.prs_tx_email, p.prs_int_tel, p.fk_end_cd_id, c.car_tx_br
 	c.car_tx_model,	c.car_tx_color,	c.car_tx_fuel_type,	c.car_dt_release, c.car_nm_price
 from pessoa p
 join carro c on p.prs_cd_id = c.fk_prs_cd_id 
-where c.car_tx_fuel_type = 'Gasolina'
-order by p.prs_cd_id ;
+where c.car_tx_fuel_type like '%Gasolina%'
+group by p.prs_tx_name, p.prs_tx_email, p.prs_int_tel, p.fk_end_cd_id, c.car_tx_brand,
+	c.car_tx_model,	c.car_tx_color,	c.car_tx_fuel_type,	c.car_dt_release, c.car_nm_price order by p.prs_tx_name  ;
+create index fueltype on carro(car_tx_fuel_type);
 
 --selecionando carro pela cor
 select c.car_tx_brand, c.car_tx_model,	c.car_tx_color,	c.car_tx_fuel_type,
 c.car_dt_release, c.car_nm_price, p.prs_tx_name, p.prs_tx_email, p.prs_int_tel, p.fk_end_cd_id
 from pessoa p
 join carro c on p.prs_cd_id = c.fk_prs_cd_id 
-where c.car_tx_color = 'Branco'
-order by p.prs_cd_id ;
+where c.car_tx_color like 'Azul%'
+group by c.car_tx_brand, c.car_tx_model,	c.car_tx_color,	c.car_tx_fuel_type,
+	c.car_dt_release, c.car_nm_price, p.prs_tx_name, p.prs_tx_email, p.prs_int_tel, p.fk_end_cd_id order by c.car_tx_color  ;
+create index cor on carro(car_tx_color);
 
 --selecionando carro pelo fabricante
 select c.car_tx_brand, c.car_tx_model,	c.car_tx_color,	c.car_tx_fuel_type,
@@ -125,9 +161,9 @@ c.car_dt_release, c.car_nm_price, p.prs_tx_name, p.prs_tx_email, p.prs_int_tel, 
 from pessoa p
 join carro c on p.prs_cd_id = c.fk_prs_cd_id 
 where c.car_tx_brand = 'Subaru'
-order by p.prs_cd_id ;
-
- 
+group by c.car_tx_brand, c.car_tx_model,	c.car_tx_color,	c.car_tx_fuel_type,
+	c.car_dt_release, c.car_nm_price, p.prs_tx_name, p.prs_tx_email, p.prs_int_tel, p.fk_end_cd_id order by c.car_tx_brand  ;
+create index brand on carro(car_tx_brand);
 
 
 
